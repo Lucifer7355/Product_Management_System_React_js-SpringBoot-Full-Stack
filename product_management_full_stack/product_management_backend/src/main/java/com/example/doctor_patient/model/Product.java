253cf7ab1a671ad9078ffe2b242270faf21a01a0
Ankit <ankitviddya@gmail.com>
2022-11-14 00:09:20 +0530

    fully created a full stack react and springboot working web application

---
+++ product_management_full_stack/product_management_backend/src/main/java/com/example/doctor_patient/model/Product.java
@@ -0,0 +1,74 @@
+package com.example.doctor_patient.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Entity
+@Table(name="products")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String productName;
+
+	private String description;
+
+	private Double price;
+
+	private String status;
+	
+	
+	public Product() {
+		super();
+	}
+
+	public Product(String productName, String description, Double price, String status) {
+		super();
+		this.productName = productName;
+		this.description = description;
+		this.price = price;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
